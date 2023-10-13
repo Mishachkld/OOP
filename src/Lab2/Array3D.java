9623fb63250736a0de84d3ee44af4150fe8a4fbb
Mishachkld <mishachkld@gmail.com>
2023-10-13 23:23:29 +0200

    Lab 2

--- src/Lab2/Array3D.java
+++ src/Lab2/Array3D.java
@@ -7,7 +7,9 @@ import java.util.Vector;
 public class Array3D<T> {
 
     public static void main(String[] args) {
-        Array3D<Integer> array = new Array3D<>(2, 12);
+        //Array3D<Integer> array = new Array3D<>(2, 12);
+        List<Integer> raw = new ArrayList<>(List.of(1, 2, 2, 9,5,6,88,8,9,894,564,231,456,4856,9478,849,456,456,456,456));
+        Array3D<Integer> array = new Array3D<>(raw, 3, 3, 3);
         System.out.println(array);
         array.setItem0(1, 1, 9999);
         System.out.println(array);
@@ -17,13 +19,14 @@ public class Array3D<T> {
         System.out.println(array);
         System.out.println(array.getItemAtPosition(1, 0, 0));
         System.out.println("array.get1D 1: " + array.getValue2D(1));
-        System.out.println("array.getItemAtPosition 1 0 0: "+ array.getItemAtPosition(1, 0, 0));
-        System.out.println("array.getValues1 0 1: " + array.getValues1(0, 1) );
+        System.out.println("array.getItemAtPosition 1 0 0: " + array.getItemAtPosition(1, 0, 0));
+        System.out.println("array.getValues1 0 1: " + array.getValues1(0, 1));
         System.out.println("array.getValue1D 1 0: " + array.getValue1D(1, 0));
     }
 
     private static final int START_POSITION = 0;
-    private List<List<List<T>>> megaArray3D;
+    private final List<List<List<T>>> megaArray3D;
+    private List<T> megaArray1D = null;
 
     public Array3D(List<List<List<T>>> megaArray3D) {
         this.megaArray3D = megaArray3D;
@@ -34,6 +37,27 @@ public class Array3D<T> {
             megaArray3D = createArray(size);
             fill(size, t);
         } else throw new NegativeArraySizeException("Incorrect size!!!");
+    }
+
+    public Array3D(List<T> rawArray, int x, int y, int z) {
+        this.megaArray1D = rawArray;
+        this.megaArray3D = create2DArray(x, y, z);
+        from1Dto3DArray(x, y, z, rawArray.size());
+    }
+
+
+    public void from1Dto3DArray(int x, int y, int z, int size) {
+        //List<List<List<T>>> array3D = create2DArray();
+        int l = 0;
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < y; j++)
+                for (int k = 0; k < z; k++) {
+                    if (l < megaArray1D.size())
+                        megaArray3D.get(i).get(j).set(k, megaArray1D.get(l++));
+                    else break;
+                    ///add(new ArrayList<>(List.of(megaArray1D.get(i), megaArray1D.get(i + 1), megaArray1D.get(i + 2))));
+                }
+
     }
 
     public Array3D(int size) {
@@ -65,7 +89,7 @@ public class Array3D<T> {
     }
 
 
-    public void fill(int count, T t) {  /// c обобщениями не возможно сделать статитический метод, т.к. статческие методы инициализируются единожды на этапе компиляции
+    public void fill(int count, T t) {  /// c обобщениями не возможно сделать статитический метод, т.к. статческие методы инициализируются единожды на этапе компиляции, а мы заранее не можем знать, что будет стоять под Т
         for (List<List<T>> array2D : megaArray3D)
             for (List<T> array : array2D)
                 for (int i = 0; i < count; i++) {
@@ -84,8 +108,22 @@ public class Array3D<T> {
         return array3D;
     }
 
+    private List<List<List<T>>> create2DArray(int x, int y, int z) {
+        List<List<List<T>>> array3D = new ArrayList<>();
+        for (int i = 0; i < x; i++) {
+            array3D.add(new ArrayList<>());
+            for (int j = 0; j < y; j++) {
+                array3D.get(i).add(new ArrayList<>());
+                for (int k = 0; k < z; k++) {
+                    array3D.get(i).get(j).add(null);
+                }
+            }
+        }
+        return array3D;
+    }
+
     private boolean checkerOfIndex(int i, int j, int k) {
-        return (this.megaArray3D.size() > i) && (this.megaArray3D.get(i).size() > j) && (this.megaArray3D.get(i).get(j).size() > k);
+        return (this.megaArray3D.size() > i) && (this.megaArray3D.get(i).size() > j) && (this.megaArray3D.get(i).get(j).size() > k) && (megaArray3D.get(i).get(j).get(k) != null);
     }
 
     private boolean checkerSize(int size) {
@@ -100,10 +138,9 @@ public class Array3D<T> {
         return getValue1D(i, k);
     }
 
-    public List<T> getItem12(int j , int k) {
+    public List<T> getItem12(int j, int k) {
         return getValue1D(j, k);
     }
-
 
 
     public T getItemAtPosition(int i, int j, int k) {
@@ -182,7 +219,8 @@ public class Array3D<T> {
             strBuilder.append(counter++).append(". \n");
             for (List<T> array : array2D) {
                 for (T item : array) {
-                    strBuilder.append(item).append(" ");
+                    if (item != null)
+                        strBuilder.append(item).append(" ");
                 }
                 strBuilder.append("\n");
             }
