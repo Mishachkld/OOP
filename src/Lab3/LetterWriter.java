a7831accb6c6399c2a2eb643902b6b3d2080ec6e
Mishachkld <mishachkld@gmail.com>
2023-10-28 14:59:32 +0200

    lab3 ready LetterWriter

--- src/Lab3/LetterWriter.java
+++ src/Lab3/LetterWriter.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class LetterWriter {
 
     public enum ConsoleColor {
-        YELLOW("\033[0m"), BLUE("\033[0;34m"), PURPLE("\033[0;35m");
+        YELLOW("\u001B[33m"), BLUE("\033[0;34m"), PURPLE("\033[0;35m");
 
-        private String color;
+        private final String color;
 
         ConsoleColor(String color) {
             this.color = color;
@@ -27,12 +27,11 @@ public class LetterWriter {
 
     public static void main(String[] args) {
         LetterWriter writer = LetterWriter.getLetterWriter();
-        writer.writeTextToArray(ConsoleColor.YELLOW, "HELLO AMIGO");
+        writer.writeText(ConsoleColor.YELLOW, "HELLO AMIGO");
+        writer.setBigFont();
+        writer.writeText(ConsoleColor.PURPLE, "ANOTHER AMIGO");
 //        HashMap<String, List<String>> hashMap = new HashMap<>();
     }
-
-
-    private int[] tecCoordinates = {0, 0};
 
     private LetterWriter() {
         alphabet = new ArrayList<>();
@@ -44,9 +43,18 @@ public class LetterWriter {
 
     }
 
+    public void setBigFont(){
+        try {
+            isBigFont = !isBigFont;
+            alphabet = new ArrayList<>();
+            getLetters();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     private List<List<String>> readFile() throws IOException {
         String path = "D:\\Development\\JavaProject\\Desktop\\OOP labs\\src\\Lab3\\5.txt";
-        if (!isBigFont) {
+        if (isBigFont) {
             path = "D:\\Development\\JavaProject\\Desktop\\OOP labs\\src\\Lab3\\7.txt";
         }
         List<List<String>> letters = new ArrayList<>();
@@ -97,7 +105,7 @@ public class LetterWriter {
         alphabet.add(new Letter(" ", str));
     }
 
-    public void writeTextToArray(ConsoleColor consoleColor, String text) {
+    public void writeText(ConsoleColor consoleColor, String text) {
         List<String> splitText = List.of(text.split(""));
         List<Letter> textArray = new ArrayList<>();
         if (checkText(text)) {
@@ -110,17 +118,16 @@ public class LetterWriter {
                 }
             }
         } else throw new RuntimeException("Incorrect world!!!");
-        outText(textArray);
+        outText(consoleColor, textArray);
     }
 
-    private void outText(List<Letter> textArray){
-        List<String> outText = new ArrayList<>();
+    private void outText(ConsoleColor consoleColor, List<Letter> textArray){
         for (int i = 0; i < alphabet.get(0).getArrayLetter().size(); i++) {
             StringBuilder builder = new StringBuilder();
             for (Letter item : textArray) {
                 builder.append(item.getArrayLetter().get(i)).append(" ");
             }
-            System.out.println(builder);
+            System.out.println(consoleColor.getColor() + builder.toString());
         }
     }
 
@@ -135,33 +142,8 @@ public class LetterWriter {
         return letterWriter;
     }
 
-    public boolean checkCoordinates(int x, int y) {
+   /* public boolean checkCoordinates(int x, int y) {
         return (x >= 0) && (y >= 0);
     }
-
-
-    public int getTecCoordinatesX() {
-        return tecCoordinates[0];
-    }
-
-    public int getTecCoordinatesY() {
-        return tecCoordinates[1];
-    }
-
-
-    public static class ColorsForConsole {
-
-        // Reset
-        public static final String RESET = "\033[0m";  // Text Reset
-
-        // Regular Colors
-        public static final String BLACK = "\033[0;30m";   // BLACK
-        public static final String RED = "\033[0;31m";     // RED
-        public static final String GREEN = "\033[0;32m";   // GREEN
-        public static final String YELLOW = "\033[0;33m";  // YELLOW
-        public static final String BLUE = "\033[0;34m";    // BLUE
-        public static final String PURPLE = "\033[0;35m";  // PURPLE
-        public static final String CYAN = "\033[0;36m";    // CYAN
-        public static final String WHITE = "\033[0;37m";   // WHITE
-    }
+    */
 }
